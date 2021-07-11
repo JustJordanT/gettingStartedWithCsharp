58663938f55042b73d946ffbbaf8ee2944b51ec1
Jordan T <Jordantay9014@gmail.com>
2021-07-11 09:50:25 -0500

    Add files via upload

---
+++ gettingStartedWIthCsharp - stringFormatting/stringFormat/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace stringFormat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter you first name: ");
+            string firstName = Console.ReadLine();
+            var upperFirstName = firstName.ToUpper();
+
+            Console.WriteLine("Enter you last name: ");
+            string lastName = Console.ReadLine();
+            var upperLastName = lastName.ToUpper();
+
+            Console.WriteLine($"Your initials are: '{upperFirstName[0]}.{upperLastName[0]}'");
+        }
+    }
+}
